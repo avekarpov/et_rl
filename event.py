3896af764ecf2d4c7e7197a77e33398d73624f41
Artem Karpov <artem.ka1001pm@gmail.com>
2024-11-09 12:06:55 +0300

    Add event handler

--- event.py
+++ event.py
@@ -20,3 +20,20 @@ class Event:
     def __str__(self):
         return f'{{"name": "{self.name}", "value": {self.value}, "ts": {self.ts}}}'
         
+class EventHandler:
+    def process_event(self, event):
+        if event.name == 'order_book':
+            return self.process_event_order_book(event)
+        elif event.name == 'trade':
+            return self.process_event_trade(event)
+        elif event.name == 'place_market':
+            return self.prorcess_event_place_market(event)
+        
+    def process_event_order_book(self, event):
+        pass
+
+    def process_event_trade(self, event):
+        pass
+
+    def prorcess_event_place_market(self, event):
+        pass
